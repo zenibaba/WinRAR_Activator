11a77b7e5730c1b2dd0577c52f5f00f9ad41bae9
Captain Fahim <205505740+CaptainFahim7@users.noreply.github.com>
2025-09-19 14:43:41 +0600

    WAS.ps1

--- WAS.ps1
+++ WAS.ps1
@@ -1,7 +1,7 @@
 # Enable TLSv1.2 for compatibility with older clients
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
 
-$DownloadURL = 'https://naeembolchhi.github.io/WinRAR-Activator/WRA.cmd'
+$DownloadURL = 'https://github.com/CaptainFahim7/WinRAR_Activator/blob/main/WAS.cmd'
 
 $FilePath = "$env:TEMP\WRA.cmd"
 
@@ -16,4 +16,5 @@ if (Test-Path $FilePath) {
     Start-Process $FilePath -Wait
     $item = Get-Item -LiteralPath $FilePath
     $item.Delete()
+
 }
